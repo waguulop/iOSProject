e316da1c86e0eb5f677452f5ddee8546c91c9358
waguulop <guillermosqgr@gmail.com>
2020-03-05 10:47:41 +0100

    Initial Commit

---
+++ SwiftApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftApp
+//
+//  Created by Guillermo Santamaría Quesada on 05/03/2020.
+//  Copyright © 2020 Guillermo Santamaría Quesada. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
